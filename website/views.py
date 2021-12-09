0154a79e7d0d17cc570a647f5d4f549a0fade6eb
Francois Bilodeau <fbilo22@gmail.com>
2021-12-09 16:37:07 -0500

    Large front-end update

--- website/views.py
+++ website/views.py
@@ -5,3 +5,19 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("home.html")
+
+@views.route('/recipes')
+def recipes_view():
+    return render_template("recipes.html")
+
+@views.route('/sessions')
+def sessions_view():
+    return render_template("sessions.html")
+
+@views.route('/recipe-page')
+def recipe_page_view():
+    return render_template("recipe-page.html")
+
+@views.route('/session-page')
+def session_page_view():
+    return render_template("session-page.html")
